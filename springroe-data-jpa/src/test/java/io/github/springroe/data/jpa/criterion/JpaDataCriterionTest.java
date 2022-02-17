6deac2eb62988d6987704ddff358fd8fcc9fe86d
kongsiyu <siyu.kong@leiyankeji.com>
2022-02-17 12:07:53 +0800

    test(jpa): 添加JpaDataCriterionTest单元测试模板

---
+++ springroe-data-jpa/src/test/java/io/github/springroe/data/jpa/criterion/JpaDataCriterionTest.java
@@ -0,0 +1,68 @@
+package io.github.springroe.data.jpa.criterion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JpaDataCriterionTest {
+
+    @Test
+    void getEntityManager() {
+    }
+
+    @Test
+    void findAllByProperty() {
+    }
+
+    @Test
+    void findAllInProperty() {
+    }
+
+    @Test
+    void testFindAllInProperty() {
+    }
+
+    @Test
+    void testFindAllInProperty1() {
+    }
+
+    @Test
+    void getByProperty() {
+    }
+
+    @Test
+    void findFirst() {
+    }
+
+    @Test
+    void getPath() {
+    }
+
+    @Test
+    void testGetPath() {
+    }
+
+    @Test
+    void findOneNotEmpty() {
+    }
+
+    @Test
+    void testFindFirst() {
+    }
+
+    @Test
+    void findFirstNotEmpty() {
+    }
+
+    @Test
+    void findOne() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void testFindAll() {
+    }
+}
