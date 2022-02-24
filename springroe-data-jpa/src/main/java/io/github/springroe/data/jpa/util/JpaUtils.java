513226d67f0d8939de5d990c44ef92a1752af8d7
kongsiyu <siyu.kong@leiyankeji.com>
2022-02-24 17:37:37 +0800

    fix(jpa): 修复错误的class引用

--- springroe-data-jpa/src/main/java/io/github/springroe/data/jpa/util/JpaUtils.java
+++ springroe-data-jpa/src/main/java/io/github/springroe/data/jpa/util/JpaUtils.java
@@ -1,6 +1,6 @@
 package io.github.springroe.data.jpa.util;
 
-import io.github.springroe.data.core.entity.Entity;
+import io.github.springroe.data.core.domain.Persistable;
 import io.github.springroe.data.jpa.domain.OneToManyAttributeData;
 
 import javax.persistence.EntityManager;
@@ -13,7 +13,6 @@ public class JpaUtils {
 
 
     /**
-     *
      * @param entityManager
      * @param query
      * @param root
@@ -26,7 +25,6 @@ public class JpaUtils {
 
 
     /**
-     *
      * @param jpaUtils
      * @param entityManager
      * @param criteriaQuery
@@ -44,7 +42,6 @@ public class JpaUtils {
     }
 
     /**
-     *
      * @param entityClass
      * @param tuple
      * @param selections
@@ -54,7 +51,7 @@ public class JpaUtils {
      * @param <T>
      * @return
      */
-    public final <T extends Entity<?>> T buildEntity(Class<T> entityClass, Tuple tuple, List<? extends Selection<?>> selections, List<? extends Selection<?>> baseSelections, Map<Join<?, ?>, ? extends List<? extends Path<?>>> toOneJoinMap, List<OneToManyAttributeData> oneToManyDataList) {
+    public final <T extends Persistable<?>> T buildEntity(Class<T> entityClass, Tuple tuple, List<? extends Selection<?>> selections, List<? extends Selection<?>> baseSelections, Map<Join<?, ?>, ? extends List<? extends Path<?>>> toOneJoinMap, List<OneToManyAttributeData> oneToManyDataList) {
         return null;
     }
 
