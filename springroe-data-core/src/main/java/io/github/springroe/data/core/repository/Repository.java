7329d21b597ec9fdbd11d49cc1770866d2825aa9
kongsiyu <siyu.kong@leiyankeji.com>
2022-05-07 16:36:04 +0800

    fix: CrudRepository&SpringDataCriterion methods duplicate in Repository

--- springroe-data-core/src/main/java/io/github/springroe/data/core/repository/Repository.java
+++ springroe-data-core/src/main/java/io/github/springroe/data/core/repository/Repository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 /**
  * A super base of repository <br/>
@@ -19,4 +20,20 @@ import java.io.Serializable;
 public interface Repository<T extends Persistable<ID>, ID extends Serializable> extends PagingAndSortingRepository<T, ID>,
         DataCriterion<T, ID>,
         CrudRepository<T, ID> {
+
+
+    @Override
+    <S extends T> S save(S entity);
+
+    @Override
+    Optional<T> findById(ID id);
+
+    @Override
+    boolean existsById(ID id);
+
+    @Override
+    void deleteById(ID id);
+
+    @Override
+    void delete(T entity);
 }
